3b13dba5ab76aab736959cec57775e3defea665e
Julia Cordero <jrcorder@usc.edu>
2021-05-09 18:22:07 -0700

    logging

--- public/js/log.js
+++ public/js/log.js
@@ -1,13 +1,16 @@
 class Log {
 	constructor() {
 		this.db = firebase.firestore();
-		this.timestampToDocumentData = new Map();
-		// Testing logging
-		this.db.collection('users').add({
-		    first: "Ada",
-		    last: "Lovelace",
-		    born: 1815
-		})
+		// Participant IP address fetch
+		this.IPAddress = "";
+		let apiKey = '97da8bc6edef8922e8332eef9df13875c2cb3dbdb8175607d62c5c67';
+		this.json("https://api.ipdata.co?api-key=" + apiKey).then(data => {
+		 	this.IPAddress = data.ip;
+			this.db.collection(this.IPAddress).doc("initial").set({
+			    creationTime: Date.now(),
+			})
+		});
+		this.count = 0;
 	}
 
 	update(time, landmarks, poseDetected) {
@@ -20,7 +23,18 @@ class Log {
 		    }
 		}
 		flattenedData.set('poseDetected', poseDetected);
-		this.timestampToDocumentData.set(time, flattenedData);
+		this.db.collection(this.IPAddress).doc(time.toString()).set(this.strMapToObj(flattenedData));
+	}
 
+	json(url) {
+	  return fetch(url).then(res => res.json());
+	}
+
+	strMapToObj(strMap) {
+	  let obj = Object.create(null);
+	  for (let [k,v] of strMap) {
+	    obj[k] = v;
+	  }
+	  return obj;
 	}
 }
