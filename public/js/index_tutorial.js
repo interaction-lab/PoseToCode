67a4fcee12209dc7b40c127751d5b890f4b7424c
Chloe Kuo <cmkuo@usc.edu>
2021-12-04 13:01:08 -0800

    Added regex for all challenges

--- public/js/index_tutorial.js
+++ public/js/index_tutorial.js
@@ -666,7 +666,12 @@ function stepCode() {
   var regex_challenge1 = /highlightBlock\(.*\);[\r\n]raiseTheRoof\(\);/ //TODO: replace with regex for multiple dance move blocks
   var regex_challenge2 = /highlightBlock\(.*\);[\r\n]makeLargeSphere\(\);[\r\n]highlightBlock\(.*\);[\r\n]placeSphereCode\(\);[\r\n]highlightBlock\(.*\);[\r\n]makeMediumSphere\(\);[\r\n]highlightBlock\(.*\);[\r\n]placeSphereCode\(\);[\r\n]highlightBlock\(.*\);[\r\n]makeSmallSphere\(\);[\r\n]highlightBlock\(.*\);[\r\n]placeSphereCode\(\);/
   var regex_challenge3 = /highlightBlock\(.*\);[\r\n]makeLargeLayer\(\);[\r\n]highlightBlock\(.*\);[\r\n]placeLayer\(\);[\r\n]highlightBlock\(.*\);[\r\n]frostLayer\(\);[\r\n]highlightBlock\(.*\);[\r\n]makeSmallLayer\(\);[\r\n]highlightBlock\(.*\);[\r\n]placeLayer\(\);[\r\n]highlightBlock\(.*\);[\r\n]frostLayer\(\);/
-  if (latestCode.match(regex_challenge1)) {
+  const count = (str) => {
+    const re = /\(\)/g
+    return ((str || '').match(re) || []).length
+  }
+  if (count(latestCode) == 4) {
+  //if (latestCode.match(regex_challenge1)) {
     completedChallenge1 = true;
   }
   if (completedChallenge1 && latestCode.match(regex_challenge2)) {
